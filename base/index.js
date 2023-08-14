dc77acf4b8427ec36c8e42711abf86cf2ee73066
Matthias Hecht <matthias.hecht@boehringer-ingelheim.com>
2023-08-14 15:04:25 +0200

    feat: update to typescript-eslint@6
    - Migrate to the new ruleset "recommended-type-checked" in base
    - Migrate to the new ruleset "strict-type-checked" in strict
    - Add "stylistic-type-checked" to base
    
    BREAKING CHANGE: update to typescript-eslint@6

--- base/index.js
+++ base/index.js
@@ -11,8 +11,8 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:import/recommended",
     "plugin:import/typescript",
     "plugin:sonarjs/recommended",
