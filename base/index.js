00939c646965d72a93b7716d610166ed90f3bd60
Simon Golms <development@gol.ms>
2024-07-25 13:36:18 +0200

    feat(base): add 'eslint-plugin-deprecation' to report usage of deprecated code
    BREAKING CHANGE: add 'eslint-plugin-deprecation' to base configuration

--- base/index.js
+++ base/index.js
@@ -15,6 +15,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:deprecation/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:perfectionist/recommended-natural',
