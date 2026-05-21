e2db552b6d6fe526dedaba804fb5419f174e39b2
Hecht,Matthias (IT EDP) BIG-DE-B <matthias.hecht@boehringer-ingelheim.com>
2026-05-21 10:30:34 +0200

    chore: adjust lintstaged setup

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,6 @@
 export default {
-  '*.json': ['prettier --write', 'git add --force'],
-  '*.{js,ts}': [
+  '!*.{*js,*ts}': ['prettier --ignore-unknown --write', 'git add --force'],
+  '*.{*js,*ts}': [
     // Extend rule set of .eslintrc.js with 'no-console'
     "eslint --fix --rule 'no-console: [error, { allow: [warn, error] }]'",
     'git add --force',
